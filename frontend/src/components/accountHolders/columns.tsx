46ed1d3ec976c330a5e19a8eeb3d651c02968d43
biniyam yoseph <biniyamyoseph063@gmail.com>
2025-11-18 09:55:10 +0300

    data table modification

--- frontend/src/components/accountHolders/columns.tsx
+++ frontend/src/components/accountHolders/columns.tsx
@@ -1,10 +1,30 @@
 import type { ColumnDef } from "@tanstack/react-table";
 import type { AccountHolder } from "types";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const columns: ColumnDef<AccountHolder>[] = [
   {
     accessorKey: "name",
-    header: "Name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "accountNumber",
@@ -101,6 +121,37 @@ export const columns: ColumnDef<AccountHolder>[] = [
         currency: "USD",
       }).format(repAmount);
       return <div className="text-right font-medium">{formatted}</div>;
+    },
+  },
+
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const account = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() =>
+                navigator.clipboard.writeText(account.accountNumber)
+              }
+            >
+              Copy Account No
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
   },
 ];
